f28b5226a4e7a647ef1c4194c6199b4a9944473e
malykhs1 <malykhs1995@yandex.ru>
2023-03-13 15:11:56 +0200

    initial commit

---
+++ fedex/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { FedexForm } from './components/FedexForm';
+
+function App() {
+  return (
+    <div className="App">
+      <FedexForm />
+    </div>
+  );
+}
+
+export default App;
